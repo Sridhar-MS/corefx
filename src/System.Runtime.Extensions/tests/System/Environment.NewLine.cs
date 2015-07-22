c998552cea400073bcdeca909dc580551ef16026
Sridhar Periyasamy <sridhper@microsoft.com>
2015-07-21 19:17:27 -0700

    Add missing unit tests for System.Environment class
    The following pulbic APIs from System.Enironment did not have any unit test coverage
    - Environment.NewLine.cs
    - Environment.ProcessorCount.cs
    - Environment.TickCount.cs

---
+++ src/System.Runtime.Extensions/tests/System/Environment.NewLine.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace System.Runtime.Extensions.Tests.System
+{
+    public class EnvironmentNewLine
+    {
+        [Fact]
+        public void NewLineTest()
+        {
+            string expectedNewLine = Interop.IsWindows ? "\r\n" : "\n";
+            Assert.Equal(expectedNewLine, Environment.NewLine);
+        }
+    }
+
+
+
+
+}
---
+++ src/System.Runtime.Extensions/tests/System/Environment.TickCount.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace System.Runtime.Extensions.Tests.System
+{
+    public class EnvironmentTickCount
+    {
+        [Fact]
+        public void TickCountTest()
+        {
+            int start = Environment.TickCount;
+            int milliSeconds = 1000;
+            Task.Delay(milliSeconds).Wait();
+            int end = Environment.TickCount;
+            Console.WriteLine("Start - " + start);
+            Console.WriteLine("End - " + end);            
+            Assert.True(end - start >= milliSeconds);
+        }
+    }
+
+
+
+
+}
